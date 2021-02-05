8d376672da6346dd9da91738d9b6232d5d9a37cf
Jackie Yang <valkjsaaa@users.noreply.github.com>
2021-02-04 22:58:20 -0800

    Fix when libc.h does not exist

--- safer_str_func_fix.h
+++ safer_str_func_fix.h
@@ -4,12 +4,14 @@
 #ifndef DANMAKUFACTORY_SAFER_STR_FUNC_FIX_H
 #define DANMAKUFACTORY_SAFER_STR_FUNC_FIX_H
 
-#include <libc.h>
 #include <string.h>
 
 #ifdef __STDC_LIB_EXT1__
 #define  __STDC_WANT_LIB_EXT1__ 1
 #else
+typedef int rsize_t;
+typedef int errno_t;
+
 errno_t strcat_s(char *restrict dest, rsize_t destsz, const char *restrict src);
 #endif
 
